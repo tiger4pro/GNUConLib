232f51e8d92bf9d572cec753ececb3d87561a1a7
tiger4 <tiger4_tiger4@protonmail.com>
2023-01-01 22:01:40 -0500

    ajouter un paramètre -man qui lorsque l'utilisation l'utilise affiche un texte qui explique le fonctionnement du programme et de ses paramètres.Faire en sorte que le -man puissent être utiliser sans fichier d'entrée et de sortie. Utiliser le texte français de la license GNU 3.0

--- GNUConLib.py
+++ GNUConLib.py
@@ -4,27 +4,35 @@
 # GNUConLib - Convertir un fichier wav en mp3
 # Copyright (C) 2020 Steve Prud'Homme <sprudhom@gmail.com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Ce programme est un logiciel libre ; vous pouvez le redistribuer ou le
+# modifier suivant les termes de la Licence Publique Générale GNU telle que
+# publiée par la Free Software Foundation ; soit la version 3 de la
+# licence, soit (à votre gré) toute version ultérieure.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS
+# AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISABILITÉ
+# ni d'ADÉQUATION À UN OBJECTIF PARTICULIER. Consultez la Licence Publique
+# Générale GNU pour plus de détails.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Vous devriez avoir reçu une copie de la Licence Publique Générale GNU
+# avec ce programme ; si ce n'est pas le cas, consultez
+# <https://www.gnu.org/licenses/>.
 
 import argparse
 import sys
 from pydub import AudioSegment
 
 # Définition des arguments de la ligne de commande
-parser = argparse.ArgumentParser(description="Convertir un fichier wav en mp3")
-parser.add_argument("fichier_entree", help="fichier wav à convertir")
-parser.add_argument("fichier_sortie", help="fichier mp3 résultant")
+parser = argparse.ArgumentParser(
+    description="Convertir un fichier wav en mp3",
+    epilog="""Exemples :
+    python GNUConLib.py fichier.wav fichier.mp3
+    python GNUConLib.py fichier.wav fichier.mp3 --channels 1 --sample_rate 44100 --bitrate 128""",
+    usage="%(prog)s [-man] fichier_entree.wav fichier_sortie.mp3 [--channels N] [--sample_rate N] [--bitrate N]",
+)
+parser.add_argument("-man", action="store_true", help="afficher l'aide")
+parser.add_argument("fichier_entree", nargs='?', help="fichier wav à convertir")
+parser.add_argument("fichier_sortie", nargs='?', help="fichier mp3 résultant")
 parser.add_argument("--channels", type=int, default=2, help="nombre de canaux (1 pour mono, 2 pour stéréo)")
 parser.add_argument("--sample_rate", type=int, default=44100, help="fréquence d'échantillonnage en Hz")
 parser.add_argument("--bitrate", type=int, default=320, help="débit en kbps")
@@ -32,8 +40,10 @@ parser.add_argument("--bitrate", type=int, default=320, help="débit en kbps")
 # Parse les arguments de la ligne de commande
 args = parser.parse_args()
 
-# Récupère le nom des fichiers d'entrée et de sortie
-fichier_entree = args.fichier_entree
+# Affiche l'aide si le paramètre -man est utilisé
+if args.man:
+    parser.print_help()
+    sys.exit(0)
 
 # Récupère le nom des fichiers d'entrée et de sortie
 fichier_entree = args.fichier_entree
